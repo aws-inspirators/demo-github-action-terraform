c876471f6ebf14994adf771a1514786c06746c09
iamdibo365 <ngoh.dibongo@gmail.com>
2023-06-02 14:03:17 -0500

    added-modified

--- main.tf
+++ main.tf
@@ -74,10 +74,6 @@ data "aws_vpc" "default_vpc" {
   default = true
 }
 
-data "aws_subnet_ids" "default_subnet" {
-  vpc_id = data.aws_vpc.default_vpc.id
-}
-
 resource "aws_security_group" "instances" {
   name = "instance-security-group"
 }
@@ -214,7 +210,7 @@ resource "aws_db_instance" "db_instance" {
   engine               = "postgres"
   engine_version       = "14"
   instance_class       = "db.t4g.large"
-  name                 = "mydb"
+  name                 = "dibo-db"
   username             = "foo"
   password             = "foobarbaz"
   skip_final_snapshot  = true
