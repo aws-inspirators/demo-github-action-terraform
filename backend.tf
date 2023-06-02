556513dfab0d8b2092fec3ab22a9bbbffa4472ce
iamdibo365 <ngoh.dibongo@gmail.com>
2023-06-02 14:00:53 -0500

    added-modified

--- backend.tf
+++ backend.tf
@@ -1,7 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "dibo-directive-tf-state"
-    key    = "actions/terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "dibo-directive-tf-state"
+    key            = "actions/ci/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
   }
 }
